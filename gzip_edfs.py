0b84734be93cf975d043627fc791bff06a3d4fad
Jonathan Wright <wright@esrf.fr>
2019-06-18 11:38:43 +0200

    fix py3 & import

--- gzip_edfs.py
+++ gzip_edfs.py
@@ -1,5 +1,5 @@
-
-import gzip, glob
+from __future__ import print_function
+import gzip, glob, os
 for fname in glob.glob("*.edf"):
     data = open(fname, "rb").read()
     with gzip.GzipFile(fname+".gz",mode="wb",compresslevel=4) as g:
